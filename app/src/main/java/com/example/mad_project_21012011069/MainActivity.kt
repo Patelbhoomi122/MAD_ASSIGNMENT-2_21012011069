bfda507b52b76107cbec7bc5e148b34931c2898d
hp <patelbhoomi3203@gmail.com>
2023-10-29 11:17:03 +0530

    flag game

--- app/src/main/java/com/example/mad_project_21012011069/MainActivity.kt
+++ app/src/main/java/com/example/mad_project_21012011069/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.mad_project_21012011069
 
+
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.mad_project_21012011069.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
